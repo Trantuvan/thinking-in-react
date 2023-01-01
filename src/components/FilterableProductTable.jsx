c06433918ef4fbf92e580555c07bf9bef6a2edac
trantuvan <trantuvan.kan@gmail.com>
2023-01-01 16:58:06 +0700

    add gh-pages to deploy

--- src/components/FilterableProductTable.jsx
+++ src/components/FilterableProductTable.jsx
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import SearchBar from './SearchBar';
+import React, { Component } from 'react';
 import ProductTable from './ProductTable';
+import SearchBar from './SearchBar';
 
 export default class FilterProductTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false,
+    };
+  }
+
   render() {
     const { products } = this.props;
+    const { filterText, inStockOnly } = this.state;
 
     return (
       <div className="bg-white p-8 shadow-sm rounded">
-        <SearchBar />
-        <ProductTable products={products} />
+        <SearchBar filterText={filterText} inStockOnly={inStockOnly} />
+        <ProductTable products={products} filterText={filterText} inStockOnly={inStockOnly} />
       </div>
     );
   }
--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import ProductCategoryRow from './ProductCategoryRow';
 import ProductRow from './ProductRow';
 
